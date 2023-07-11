feabc6ef883653938a95330a0e3b50d8a7b4a2aa
igarashi <kariyou12@outlook.jp>
2023-07-11 10:51:19 +0900

    コメント

--- src/News.js
+++ src/News.js
@@ -1,54 +1,50 @@
 import { useState, useEffect } from "react";
 
 /**
- * NewsApiからJSON情報を取得したコンポーネント
+ * NewsApiからJSON情報を取得し、表示するコンポーネント
  * @author 五十嵐
- * @returns App,js
+ * @returns ニュースデータを表示するためのjs
  * @version 1,0
  */
 
-/**
- * propsを使用し、変数Newsに値を格納
- */
+//propsに引数を設定
 function News(props) {
+  //取得データを保持するための設定
   const [title, setTitle] = useState('');
   const [news, setNews] = useState('');
   const [imgSrc, setImgSrc] = useState('');
 
+  //useEffectの設定
   useEffect(() => {
     
-    /**
-     * 変数newUrlにJSONのURLを格納
-     */
+    //定数newUrlにJSONのURLを格納
     const newsUrl =
-      'https://newsapi.org/v2/top-headlines?country=jp&apiKey=8932d7691175436ea3d6ba7ba5721524';
+      'https://newsapi.org/v2/top-headlines?country=jp&apiKey=d8c819299df24d048262832b733aef1c';
 
-      /**
-       * 変数newUrを読み込みJSON情報を取得、以下の情報をfetchNewsDataに格納
-       */
+      //定数newUrを読み込みJSON情報を取得、以下の関数をfetchNewsDataに格納
     const fetchNewsData = () => {
       fetch(newsUrl)
         .then(response => response.json())
         .then(data => {
-          /**
-           * JSON内の各情報を変数articleNoに格納
-           */
+          //JSON内の各情報を定数articleNoに格納
           const articleNo = props.articleNo;
           setTitle(data.articles[articleNo].title);
           setImgSrc(data.articles[articleNo].urlToImage);
           setNews(data.articles[articleNo].description);
         })
+       //エラー処理の場合
         .catch(error => {
           console.log(error);
         });
     };
 
+    //関数実行
     fetchNewsData();
 
-    /**
-     * timerを使い10秒のインターバルでfetchNewsDataの情報を更新
-     */
+    //timerを使い1時間のインターバルでfetchNewsDataの情報を更新
     const timer = setInterval(fetchNewsData, 3600000);
+  
+   //クリーンアップ処理
     return()=>{
         clearInterval(timer);
     };
@@ -56,29 +52,33 @@ function News(props) {
   }, [props.articleNo]);
 
 
-  /**
-   * JSONで取得した情報内にnullがあった場合、デフォルトの情報を表示する記述
-   */
+
   const renderTitle = () => {
     if (news) {
+       //設定した値に情報が入っていた場合、定数renderTitleに情報をリターンする
       return <h1 className="newsTitle">{title}</h1>
     } else {
+      //それ以外の場合はNo Titleを定数renderTitleにリターンする
       return <h1>No Title</h1>
     }
   };
 
   const renderImage = () => {
     if (imgSrc) {
+        //設定した値に情報が入っていた場合、定数renderImageに情報をリターンする
       return <img src={imgSrc} className="newsImage" alt="" style={{ width: '100%', height: '100%' }} />;
     } else {
+      //それ以外の場合は設定したイメージ画像を定数renderImageにリターンする
       return <img src="https://counity.blob.core.windows.net/counitysale/noimage.png" className="newsImage" alt="ノーイメージ" style={{ width: '60%', height: '60%' }} />;
     }
   };
 
   const renderText = () => {
     if (news) {
+      //設定した値に情報が入っていた場合、定数renderTextに情報をリターンする
       return <h1 className="newsText">{news}</h1>
     } else {
+      //それ以外の場合は設定したNo Textを定数renderTextにリターンする
       return <h1>No Text</h1>
     }
   };
@@ -92,4 +92,5 @@ function News(props) {
   );
 }
 
+//コンポーネントとしてexportする
 export default News;
