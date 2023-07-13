7587767d58b1d6d585aff10f7862bb6183abf528
aoki <aaookk11@outlook.jp>
2023-07-13 10:07:17 +0900

    時間変更

--- src/Pokemon.js
+++ src/Pokemon.js
@@ -48,7 +48,7 @@ const Pokemon = () => {
 
         //ポケモンのランダム取得時間をカルーセルが一巡するときに設定
         fetchPokemonData();
-        const interval = setInterval(fetchPokemonData, 10000);
+        const interval = setInterval(fetchPokemonData, 7500);
         return () => {
             clearInterval(interval);
         };
